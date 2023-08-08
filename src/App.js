dbde69c40087ac29aec4f8966f263b755871e1ed
agutierrez <agutierrez@tecnoaccion.com.ar>
2023-08-08 16:33:17 -0300

    b

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import generatedGitInfo from './generatedGitInfo.json';
 
 function App() {
-  console.log(generatedGitInfo);
+  console.log(generatedGitInfo.gitCommitHash);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.s
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={`https://github.com/ag171980/test-version/commit/${generatedGitInfo.gitCommitHash}`}
           target="_blank"
           rel="noopener noreferrer"
         >
